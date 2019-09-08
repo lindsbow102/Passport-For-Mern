8f7e30a9c2262f7c3e5d05abb246c273e4ce8ec0
Lindsey Bowen <lindsbow102@yahoo.com>
2019-09-08 14:19:22 -0700

    More front-end routing

--- client/src/App.js
+++ client/src/App.js
@@ -4,21 +4,25 @@ import Dashboard from "./components/Dashboard";
 import Signup from "./components/Signup";
 import Login from "./components/Login";
 import Profile from "./components/Profile";
+import Home from "./components/Home";
+import NotFound from "./components/NotFound";
 
 export default class App extends Component {
   render() {
     return (
       <div>
-      <Router>
-      <div>
-        <Switch>
-          <Route exact path="/dashboard" component={Dashboard} />
-          <Route exact path="/signup" component={Signup} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/profile" component={Profile} />
-        </Switch>
-      </div>
-    </Router>
+        <Router>
+          <div>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/dashboard" component={Dashboard} />
+              <Route exact path="/signup" component={Signup} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/profile" component={Profile} />
+              <Route exact path="/notfound" component={NotFound} />
+            </Switch>
+          </div>
+        </Router>
       </div>
     );
   }
---
+++ client/src/components/NotFound.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class NotFound extends Component {
+  render() {
+    return (
+      <div>
+        <h1>I am the Not Found Component</h1>
+      </div>
+    );
+  }
+}
