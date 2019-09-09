b3d028f1318f47ed21d233d966f95c96f9c6d172
Lindsey Bowen <lindsbow102@yahoo.com>
2019-09-09 07:54:31 -0700

    links to signup/login from home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,6 +5,9 @@ export default class Home extends Component {
     return (
       <div>
         <h1>I am the Home Component</h1>
+        <a href="/signup">First time users sign up here</a>
+        <br></br>
+        <a href="/login">Returning users, please log in</a>
       </div>
     );
   }
